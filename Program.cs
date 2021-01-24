a3658316f536da127ebf72a05a5f4453d8700c2f
Johnny Ernest <johnrussellernest@outlook.com>
2021-01-23 22:50:48 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -19,75 +19,139 @@ namespace LexerParser1
             Lexer lexer = new Lexer(defaultConfiguration, userConfiguration);
             Parser parser = new Parser(lexer, userConfiguration);
 
-            //string inputSql = "select column1, column2 as c2 from _yay";
-            //string inputCss = "body { background-color: #512fff; margin-left: auto; color: '#123456'; } h2 { color: 'blue'; }";
-            //string inputJs = "var i = \"Hello\"; var x=5;";
-            //string inputEBNF = "a=\"5\";";
-            //string inputEBNF2 = "PROGRAM BEGIN a:=5; b1:='Hurrooo'; b2:=\"Yay\"; b2 := a; END;";
-
-            string htmlIdentifier = @"htmlIdentifier = letter, { letter|digit|hyphen|underscore };";
-            string htmlAttribute = @"htmlAttribute = whitespaces, htmlIdentifier, { [whitespaces], ""="", [whitespaces], ebnfTerminalDoubleQuote };";
-            string htmlTagName = @"htmlTagName = htmlIdentifier;";
-            string htmlOpenTag = @"htmlOpenTag = ""<"", htmlTagName, { htmlAttribute }, [whitespaces], "">"";";
-            string htmlCloseTag = @"htmlCloseTag = ""</"", htmlTagName, "">"";";
-            string htmlInnerTagText = @"htmlInnerTagText = %% letters|spaces|digits|whitespaces|semicolon|underscore|equals %%;";
-            string htmlTag = @"htmlTag = htmlOpenTag, {htmlInnerTagText}, {htmlTag}, {htmlInnerTagText}, htmlCloseTag;";
-
-            parser.AddEBNFRule(htmlIdentifier);
-            parser.AddEBNFRule(htmlAttribute);
-            parser.AddEBNFRule(htmlTagName);
-            parser.AddEBNFRule(htmlOpenTag);
-            parser.AddEBNFRule(htmlCloseTag);
-            parser.AddEBNFRule(htmlInnerTagText);
-            parser.AddEBNFRule(htmlTag);
-
-            string inputHtml = "<html><head><title>Title</title></head><body><h2 selected>Helloooo hi</h2><div class=\"someClass\">Here is <span>some</span> text</div></body></html>";
-            var result = parser.Parse(inputHtml, sequenceName: "htmlTag", showOnConsole: false);
-            if (result.Matched)
+            string someRule = "searchRule = 'God ', letters;";
+            parser.AddEBNFRule(someRule);
+            string bibleText = @"
+1 In the beginning God created the heaven and the earth. 
+2 And the earth was without form, and void; and darkness was upon the face of the deep. And the Spirit of God moved upon the face of the waters.
+The First Day: Light
+3 And God said, Let there be light: and there was light. 
+4 And God saw the light, that it was good: and God divided the light from the darkness. 
+5 And God called the light Day, and the darkness he called Night. And the evening and the morning were the first day.
+The Second Day: Firmament
+6 And God said, Let there be a firmament in the midst of the waters, and let it divide the waters from the waters. 
+7 And God made the firmament, and divided the waters which were under the firmament from the waters which were above the firmament: and it was so.
+";
+            var search1 = parser.Search(bibleText, lexer, sequenceName: "searchRule");
+            foreach(var item in search1.Results)
             {
-                //ParserResultWalker walker = new ParserResultWalker(result.Results[0], showOnConsoleDefault: true);
-                //walker.Visit();
+                System.Diagnostics.Debug.WriteLine(item.InnerResultsText);
+            }
+            
+            //string inputJs = "var i = \"Hello\"; var x=5;";
+            //string inputPascal = "PROGRAM BEGIN a:=5; b1:='Hurrooo'; b2:=\"Yay\"; b2 := a; END;";
 
-                Console.WriteLine();
-                ConsoleWalker walker2 = new ConsoleWalker(result.Results[0]);
-                Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("Adding grammar rules for SQL");
+            string sqlGrammar = @"
+sqlIdentifier = [underscore], letters, { letters|hyphen|underscore }, [digits];
+sqlAlias = [whitespaces], 'as', whitespaces, sqlIdentifier, [whitespaces];
+sqlIdentifierListPart = [whitespaces], comma, [whitespaces], sqlIdentifier, [whitespace, sqlAlias], [whitespaces];
+sqlIdentifierList = [whitespaces], sqlIdentifier, [whitespaces, sqlAlias], [whitespaces], {sqlIdentifierListPart};
+sqlSelect = 'select', sqlIdentifierList, 'from', sqlIdentifierList;
+";
+            // ^^^ Needs case insensitive string searching for EBNF on sqlAlias and others
+            parser.AddEBNFGrammar(sqlGrammar.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries));
+            Console.WriteLine("Adding grammar rules for HTML");
+            string htmlGrammar = @"
+                htmlIdentifier = letter, { letter | digit | hyphen | underscore };
+                htmlAttribute = whitespaces, htmlIdentifier, { [whitespaces], ""="", [whitespaces], ebnfTerminalDoubleQuote };
+                htmlTagName = htmlIdentifier;
+                htmlOpenTag = ""<"", htmlTagName, { htmlAttribute }, [whitespaces], "">"";
+                htmlCloseTag = ""</"", htmlTagName, "">"";
+                htmlInnerTagText = %% letters|spaces|digits|whitespaces|semicolon|underscore|equals %%;
+                htmlTag = htmlOpenTag, {htmlInnerTagText}, {htmlTag}, {htmlInnerTagText}, htmlCloseTag;
+                ";
+            parser.AddEBNFGrammar(htmlGrammar.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries));
+            Console.WriteLine("Adding grammar rules for CSS");
+            string cssGrammar = @"
+                cssSelectorName = letters, { letters | hyphen | digits };
+                cssClass = period, cssSelectorName;
+                cssSubClass = cssClass, cssClass;
+                cssClassDescendant = cssClass, whitespace, cssClass;
+                cssID = asterisk;
+                cssID = hashMark, cssSelectorName;
+                cssElement = cssSelectorName;
+                cssElementWithClass = cssElement, cssClass;
+                cssElementList = cssElement, %% comma, [whitespace], cssElement %%;
+                cssElementInsideList = cssElement, %% whitespace, cssElement %%;
+                cssElementDescendantList = cssElement, %% [whitespace], greaterThan, [whitespace], cssElement %%;
+                cssElementPlusList = cssElement, %% [whitespace], plus, [whitespace], cssElement %%;
+                cssElementTildeList = cssElement, %% [whitespace], tilde, [whitespace], cssElement %%;
+                cssSelectors = cssSelectorName | cssClass | cssSubClass | cssClassDescendant | cssAll | cssID | cssElement | cssElementWithClass | cssElementList | cssElementInsideList | cssElementDescendantList | cssElementPlusList | cssElementTildeList;
+                cssString = { letters | digits | parenthesisOpen | parenthesisClose | hashMark | colon | forwardSlash | period | comma | strEscapedQuote | strEscapedDoubleQuote };
+                cssStringDblQuote = doubleQuote, cssString, doubleQuote;
+                cssStringQuote = quote, cssString, quote;
+                cssStringLiteral = cssStringDblQuote|cssStringQuote;
+                cssColorIdentifier = hashMark, ((hexadecimal, hexadecimal, hexadecimal, hexadecimal, hexadecimal, hexadecimal)|(hexadecimal, hexadecimal, hexadecimal)) ;
+                cssValueLiteral = letters|digits;
+                cssLiteral = cssStringLiteral|cssValueLiteral|cssColorIdentifier;
+                cssPropertyName = letters, { letters|hyphen|digits };
+                cssProperty = [whitespaces], cssPropertyName, [whitespaces], colon, [whitespaces], cssLiteral, [whitespaces], semicolon, [whitespaces];
+                cssCodeBlock = [whitespaces], braceOpen, [whitespaces], %% cssProperty, [whitespaces] %%, braceClose;
+                cssStatement = cssSelectors, %% whitespaces %%, cssCodeBlock;
+                cssStatements = [whitespaces], %% cssStatement, [whitespaces] %%;
+                ";
+            parser.AddEBNFGrammar(cssGrammar.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries));
+            Console.WriteLine("Adding grammar rules for Math Equations");
+            string mathGrammar = @"
+                mathNegative = hyphen;
+                mathDecimal = period;
+                mathNum = [mathNegative], digits, [ mathDecimal, digits ];
+                mathAdd = plus;
+                mathSubtract = hyphen;
+                mathMultiply = asterisk;
+                mathDivide = forwardSlash;
+                mathFunction = 'sqrt'|'cos'|'sin'|'tan';
+                mathParentheses = [mathFunction], parenthesisOpen, &mathExpr, parenthesisClose;
+                mathFactor = (mathNum | mathParentheses);
+                mathTerm = mathFactor, { (mathMultiply, mathFactor) | (mathDivide, mathFactor) };
+                mathExpr = mathTerm, { (mathAdd, mathTerm) | (mathSubtract, mathTerm) };";
+            parser.AddEBNFGrammar(mathGrammar.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries));
+
+            string inputSql = "select column1, column2 as c2 from _yay";
+            var resultSql = parser.Parse(inputSql, sequenceName: "sqlSelect", showOnConsole: false);
+            Console.WriteLine();
+            if (resultSql.Matched)
+            {
+                Console.Write("SQL Example: ");
+                SqlConsoleWalker walker = new SqlConsoleWalker(resultSql.Results[0]);
                 Console.WriteLine();
                 Console.WriteLine();
             }
             Console.ForegroundColor = ConsoleColor.Gray;
 
-            string mathNegative = "mathNegative = hyphen;";
-            string mathDecimal = "mathDecimal = period;";
-            string mathNum = "mathNum = [mathNegative], digits, [ mathDecimal, digits ];";
-            string mathAdd = "mathAdd = plus;";
-            string mathSubtract = "mathSubtract = hyphen;";
-            string mathMultiply = "mathMultiply = asterisk;";
-            string mathDivide = "mathDivide = forwardSlash;";
+            string inputHtml = "<html><head><title>Title</title></head><body><h2 selected>Helloooo hi</h2><div class=\"someClass\">Here is <span>some</span> text</div></body></html>";
+            var resultHtml = parser.Parse(inputHtml, sequenceName: "htmlTag", showOnConsole: false);
+            if (resultHtml.Matched)
+            {
+                //ParserResultWalker walker = new ParserResultWalker(result.Results[0], showOnConsoleDefault: true);
+                //walker.Visit();
+                Console.Write("HTML Example: ");
+                HtmlConsoleWalker walker2 = new HtmlConsoleWalker(resultHtml.Results[0]);
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine();
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
 
-            string mathFunction = "mathFunction = 'sqrt'|'cos'|'sin'|'tan';";
-
-            string mathParentheses = "mathParentheses = [mathFunction], parenthesisOpen, &mathExpr, parenthesisClose;";
-            string mathFactor = "mathFactor = (mathNum | mathParentheses);";
-            string mathTerm = "mathTerm = mathFactor, { (mathMultiply, mathFactor) | (mathDivide, mathFactor) };";
-            string mathExpr = "mathExpr = mathTerm, { (mathAdd, mathTerm) | (mathSubtract, mathTerm) };";
-
-            parser.AddEBNFRule(mathNegative);
-            parser.AddEBNFRule(mathDecimal);
-            parser.AddEBNFRule(mathNum);
-            parser.AddEBNFRule(mathAdd);
-            parser.AddEBNFRule(mathSubtract);
-            parser.AddEBNFRule(mathMultiply);
-            parser.AddEBNFRule(mathDivide);
-            parser.AddEBNFRule(mathFunction);
-            parser.AddEBNFRule(mathParentheses);
-            parser.AddEBNFRule(mathFactor);
-            parser.AddEBNFRule(mathTerm);
-            parser.AddEBNFRule(mathExpr);
+            string inputCss = "body { background-color: #512fff; margin-left: auto; color: '#123456'; } h2 { color: 'blue'; }";
+            var resultCss = parser.Parse(inputCss, sequenceName: "cssStatements", showOnConsole: false);
+            Console.WriteLine();
+            if (resultCss.Matched)
+            {
+                Console.Write("CSS Example: ");
+                CssConsoleWalker walker = new CssConsoleWalker(resultCss.Results[0]);
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
 
             string inputCalc = "((3*2.5)+1.2)+((2*7*5)*12.5)+5";
             var resultCalc = parser.Parse(inputCalc, sequenceName: "mathExpr", showOnConsole: false);
+            Console.WriteLine();
+            Console.WriteLine();
             if (resultCalc.Matched)
             {
+                Console.Write("Calculation Example: ");
+                CalculatorConsoleWalker walker = new CalculatorConsoleWalker(resultCalc.Results[0]);
+
                 var secondary = resultCalc.Results[0].Clone() as Parser.ParserResult;
                 List<Parser.ParserResult> results1 = new List<Parser.ParserResult>();
                 results1.Add(secondary);
@@ -99,7 +163,7 @@ namespace LexerParser1
                     checkGroups = new Func<Parser.ParserResult, int, Parser.ParserResult>((input1, level) =>
                     {
                         string levels = "".PadLeft(level, ' ');
-                        Console.WriteLine($"{levels}Group:{input1.GroupName}, Name:{input1.Name}, Inner:{input1.InnerResultsText}, Eval:{input1.EvaluationResult}");
+                        //Console.WriteLine($"{levels}Group:{input1.GroupName}, Name:{input1.Name}, Inner:{input1.InnerResultsText}, Eval:{input1.EvaluationResult}");
                         foreach (var g in input1.InnerResults)
                         {
                             checkGroups(g, level + 1);
@@ -107,7 +171,6 @@ namespace LexerParser1
                         return null;
                     });
 
-                    Console.WriteLine("Initial Calculation:" + inputCalc);
                     checkGroups(input, 0);
 
                     var exprs = input.GetDescendantsOfType(new string[] { "mathExpr" });
@@ -116,7 +179,7 @@ namespace LexerParser1
                     int idx = 0;
                     while (!done)
                     {
-                        Console.WriteLine("Evaluating Iteration " + idx);
+                        //Console.WriteLine("Evaluating Iteration " + idx);
 
                         List<Parser.EvaluationResult> numbers = new List<Parser.EvaluationResult>();
                         // Roll Up Numbers
@@ -150,7 +213,7 @@ namespace LexerParser1
                                 };
                                 num.InnerResults = new List<Parser.ParserResult>();
                                 numbers.Add(num.EvaluationResult);
-                                Console.WriteLine($"Number:{num}");
+                                //Console.WriteLine($"Number:{num}");
                             }
                         }
                         // Roll Up Signs
@@ -291,7 +354,7 @@ namespace LexerParser1
                             }
                         }
 
-                        Console.WriteLine(inputCalc);
+                        //Console.WriteLine(inputCalc);
                         checkGroups(input, 0);
                         if (input.InnerResults.Count == 0)
                         {
@@ -303,7 +366,7 @@ namespace LexerParser1
 
                     //Program.exprFunc(exprs.Last());
 
-                    Console.WriteLine(inputCalc);
+                    //Console.WriteLine(inputCalc);
                     
                     return new Parser.EvaluationResult()
                     {
@@ -316,9 +379,12 @@ namespace LexerParser1
                 results1[0].EvaluationFunction = funcEvaluate;
                 results1[0].EvaluationResult = results1[0].EvaluationFunction(results1[0]);
 
-                ParserResultWalker walker3 = new ParserResultWalker(results1[0], showOnConsoleDefault: true, filterOutEBNF: true);
-                walker3.Visit();
+                Console.Write("Result: ");
+                EvaluationResultConsoleWalker walker1 = new EvaluationResultConsoleWalker(results1[0].EvaluationResult);
+                //ParserResultWalker walker3 = new ParserResultWalker(results1[0], showOnConsoleDefault: true, filterOutEBNF: true);
+                //walker3.Visit();
             }
+            Console.ForegroundColor = ConsoleColor.Gray;
         }
     }
 }
